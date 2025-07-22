e9a783574d162e30ad103aec6265f5d066558d04
Kalama <97434062+Kmtengo@users.noreply.github.com>
2025-07-22 16:33:18 +0300

    Delete user_input.py

--- user_input.py
+++
@@ -1,4 +0,0 @@
-name = input("What is your name? ")
-age = input("How old are you? ")
-location = input("Where do you live? ")
-print(f"Hello {name}, you are {age} years old and live in {location}.")
